8b4da564635c676136c5c4adb81c7da909071227
Svyatoslav Plehanov <svyat.plehanov@gmail.com>
2015-12-09 14:58:10 +0200

    test tabs

--- client/src/js/controller.js
+++ client/src/js/controller.js
@@ -9,10 +9,15 @@ module.exports = function (app) {
         console.log('start controller');
         console.log(config);
 
-        var placeholder = document.getElementById('vizabi-placeholder');
+        var placeholder = document.getElementById('vizabi-placeholder1');
         var prevSlug = null;
 
-        init();
+        $scope.tab = 1;
+        $scope.setTab = function(newTab){
+          $scope.tab = newTab;
+        };
+
+        //init();
 
         if (config.isElectronApp) {
           console.log('is electron app');
@@ -58,7 +63,6 @@ module.exports = function (app) {
           $scope.loadingError = false;
           $scope.tools = {};
           $scope.validTools = [];
-          $scope.relatedItems = [];
           $scope.isWeb = !config.isElectronApp && !config.isChromeApp;
 
           //start off by getting all items
@@ -69,46 +73,46 @@ module.exports = function (app) {
           });
         }
 
-        $scope.$root.$on('$routeChangeStart', function(event, state, current){
-          var newSlug = state.params.slug;
-          if (!prevSlug) {
-            prevSlug = newSlug;
-            return;
-          }
-          if (prevSlug !== newSlug) {
-            prevSlug = newSlug;
-            // and here we go, one more hack
-            if (config.isChromeApp || config.isElectronApp) {
-              init();
-            } else {
-              setTimeout(function () {
-                window.location.reload();
-              }, 1);
-            }
-            return;
-          }
-          console.log(window.location.hash);
-        });
-        $scope.$root.$on('$routeUpdate', function(event, state){
-          var newSlug = state.params.slug;
-          if (!prevSlug) {
-            prevSlug = newSlug;
-            return;
-          }
-          if (prevSlug !== newSlug) {
-            prevSlug = newSlug;
-            // and here we go, one more hack
-            if (config.isChromeApp || config.isElectronApp) {
-              init();
-            } else {
-              setTimeout(function () {
-                window.location.reload();
-              }, 1);
-            }
-            return;
-          }
-          console.log(window.location.hash);
-        });
+        //$scope.$root.$on('$routeChangeStart', function(event, state, current){
+        //  var newSlug = state.params.slug;
+        //  if (!prevSlug) {
+        //    prevSlug = newSlug;
+        //    return;
+        //  }
+        //  if (prevSlug !== newSlug) {
+        //    prevSlug = newSlug;
+        //    // and here we go, one more hack
+        //    if (config.isChromeApp || config.isElectronApp) {
+        //      init();
+        //    } else {
+        //      setTimeout(function () {
+        //        window.location.reload();
+        //      }, 1);
+        //    }
+        //    return;
+        //  }
+        //  console.log(window.location.hash);
+        //});
+        //$scope.$root.$on('$routeUpdate', function(event, state){
+        //  var newSlug = state.params.slug;
+        //  if (!prevSlug) {
+        //    prevSlug = newSlug;
+        //    return;
+        //  }
+        //  if (prevSlug !== newSlug) {
+        //    prevSlug = newSlug;
+        //    // and here we go, one more hack
+        //    if (config.isChromeApp || config.isElectronApp) {
+        //      init();
+        //    } else {
+        //      setTimeout(function () {
+        //        window.location.reload();
+        //      }, 1);
+        //    }
+        //    return;
+        //  }
+        //  console.log(window.location.hash);
+        //});
 
         $scope.url = function(url) {
           if (config.isChromeApp || config.isElectronApp) {
@@ -119,53 +123,59 @@ module.exports = function (app) {
         };
 
         function updateGraph() {
-          var validTools = $scope.validTools;
-          if (validTools.length === 0) return;
-          if (validTools.indexOf($routeParams.slug) === -1) {
-            //redirect
-            $location.path('/' + validTools[0]);
-            return;
-          }
+          debugger;
+          //var validTools = $scope.validTools;
+          //if (validTools.length === 0) return;
+          //if (validTools.indexOf($routeParams.slug) === -1) {
+          //  //redirect
+          //  $location.path('/' + validTools[0]);
+          //  return;
+          //}
+          //$scope.activeTool = $routeParams.slug;
+          $scope.activeTool = 'bubbles';
+          // do not put data in $scope
+          var tool = angular.copy($scope.tools[$scope.activeTool]);
+          //Vizabi.clearInstances();
+          $scope.viz = vizabiFactory.render(tool.tool, placeholder, tool.opts);
+          //$scope.$apply();
+        }
 
-          scrollTo(document.querySelector('.wrapper'), 0, 200, function () {
-            $scope.activeTool = $routeParams.slug;
-            // do not put data in $scope
-            var tool = angular.copy($scope.tools[$scope.activeTool]);
+        $scope.addGraph1 = addGraph1;
+        $scope.addGraph2 = addGraph2;
+        $scope.loadingError = false;
+        $scope.tools = {};
+        $scope.validTools = [];
 
-            Vizabi.clearInstances();
+        function addGraph1() {
+          debugger;
+          console.log('add graph');
+          $scope.loadingError = false;
+          //$scope.tools = {};
+          //$scope.validTools = [];
+          placeholder = document.getElementById('vizabi-placeholder1');
 
-            if (config.isChromeApp || config.isElectronApp) {
-              //set protocol
-              tool.opts.data.path = 'http:' + tool.opts.data.path;
-              for (var i=0; i < tool.relateditems.length; i++) {
-                tool.relateditems[i].image = 'http:'+tool.relateditems[i].image;
-              }
-            }
-            $scope.viz = vizabiFactory.render(tool.tool, placeholder, tool.opts);
-            $scope.relatedItems = tool.relateditems;
-            $scope.$apply();
-
-            //send to google analytics
-            if (!config.isChromeApp && !config.isElectronApp) {
-              $window.ga('send', 'pageview', {page: $location.url()});
-            }
-
+          //updateGraph();
+          vizabiItems.getItems().then(function (items) {
+            $scope.tools = items;
+            $scope.validTools = Object.keys($scope.tools);
+            updateGraph();
           });
         }
 
-        function scrollTo(element, to, duration, cb) {
-          if (duration < 0) return;
-          var difference = to - element.scrollTop;
-          var perTick = difference / duration * 10;
+        function addGraph2() {
+          debugger;
+          console.log('add graph');
+          $scope.loadingError = false;
+          //$scope.tools = {};
+          //$scope.validTools = [];
+          placeholder = document.getElementById('vizabi-placeholder2');
 
-          setTimeout(function () {
-            element.scrollTop = element.scrollTop + perTick;
-            if (element.scrollTop == to) {
-              cb();
-              return;
-            }
-            scrollTo(element, to, duration - 10, cb);
-          }, 10);
+          //updateGraph();
+          vizabiItems.getItems().then(function (items) {
+            $scope.tools = items;
+            $scope.validTools = Object.keys($scope.tools);
+            updateGraph();
+          });
         }
       }]);
 };
