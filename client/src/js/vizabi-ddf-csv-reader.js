f0365f50263815485a3e1b96925e52726e9db322
buchslava <vyacheslav.chub@gmail.com>
2016-01-15 16:46:06 +0200

    Updated: custom readers

--- client/src/js/vizabi-ddf-csv-reader.js
+++ client/src/js/vizabi-ddf-csv-reader.js
@@ -2,20 +2,9 @@ require('d3');
 var Vizabi = require('vizabi');
 var Promise = (require('./vizabi-extract-promise')).Promise;
 var utils = require('./vizabi-extract-utils');
-var fileReaders = require('./vizabi-file-readers');
-var fileReader = fileReaders.genericReader;
-
-var FILE_CACHED = {};
-var FILE_REQUESTED = {};
-var DATA_CACHED = {};
-var CACHE = {
-  measureFileToName: {},
-  measureNameToFile: {}
-};
-
-var GEO = 1;
-var MEASURES_TIME_PERIOD = 2;
-var MEASURES_TIME_FIXED = 3;
+/*var fileReaders = require('./vizabi-file-readers');
+ var fileReader = fileReaders.genericReader;*/
+var ddfUtils = require('./vizabi-ddf-utils');
 
 Vizabi.Reader.extend('ddfcsv', {
 
@@ -46,11 +35,11 @@ Vizabi.Reader.extend('ddfcsv', {
     var _this = this;
     var query = utils.deepExtend({}, queryPar);
 
-    _this.queryDescriptor = new QueryDescriptor(queryPar);
+    _this.queryDescriptor = new ddfUtils.QueryDescriptor(queryPar);
 
-    if (_this.queryDescriptor.type === GEO) {
+    if (_this.queryDescriptor.type === ddfUtils.GEO) {
       return new Promise(function (resolve) {
-        _this.geoProcessing(1, function () {
+        ddfUtils.geoProcessing(_this._ddfPath, _this.dimensionPath, function () {
           _this._data = _this.getGeoData(_this.queryDescriptor);
           console.log('!GEO DATA', _this._data);
           resolve();
@@ -58,16 +47,16 @@ Vizabi.Reader.extend('ddfcsv', {
       });
     }
 
-    if (_this.queryDescriptor.type === MEASURES_TIME_PERIOD ||
-      _this.queryDescriptor.type === MEASURES_TIME_FIXED) {
+    if (_this.queryDescriptor.type === ddfUtils.MEASURES_TIME_PERIOD ||
+      _this.queryDescriptor.type === ddfUtils.MEASURES_TIME_FIXED) {
 
       return new Promise(function (resolve) {
-        _this.getIndex().then(function () {
+        ddfUtils.getIndex(_this.indexPath).then(function () {
           Promise
             .all(_this.getExpectedMeasures(query))
             .then(function () {
               var result = [];
-              var geo = DATA_CACHED['geo-' + _this.queryDescriptor.category];
+              var geo = ddfUtils.CACHE.DATA_CACHED['geo-' + _this.queryDescriptor.category];
 
               var d1 = (new Date(_this.queryDescriptor.timeFrom)).getFullYear();
               var d2 = (new Date(_this.queryDescriptor.timeTo)).getFullYear();
@@ -107,24 +96,24 @@ Vizabi.Reader.extend('ddfcsv', {
     return this._data;
   },
 
-  geoProcessing: function (n, cb) {
-    var _this = this;
-    _this.getDimensions().then(function () {
-      Promise
-        .all(_this.getDimensionsDetails())
-        .then(function () {
-          cb();
-        });
-    });
-  },
+  /*geoProcessing: function (n, cb) {
+   var _this = this;
+   _this.getDimensions().then(function () {
+   Promise
+   .all(_this.getDimensionsDetails())
+   .then(function () {
+   cb();
+   });
+   });
+   },*/
 
   injectMeasureValues: function (query, line, geoIndex, year) {
     var f = 0;
     var measures = this.getMeasuresNames(query);
-    var geo = DATA_CACHED['geo-' + this.queryDescriptor.category];
+    var geo = ddfUtils.CACHE.DATA_CACHED['geo-' + this.queryDescriptor.category];
 
     measures.forEach(function (m) {
-      var measureCache = FILE_CACHED[CACHE.measureFileToName[m]];
+      var measureCache = ddfUtils.CACHE.FILE_CACHED[ddfUtils.CACHE.measureFileToName[m]];
 
       if (measureCache && measureCache[geo[geoIndex].geo]) {
         if (measureCache[geo[geoIndex].geo] && measureCache[geo[geoIndex].geo][year + ''] &&
@@ -138,13 +127,13 @@ Vizabi.Reader.extend('ddfcsv', {
     return f === measures.length;
   },
 
-  getIndex: function () {
-    return this.load(this.indexPath);
-  },
+  /*getIndex: function () {
+   return this.load(this.indexPath);
+   },
 
-  getDimensions: function () {
-    return this.load(this.dimensionPath);
-  },
+   getDimensions: function () {
+   return this.load(this.dimensionPath);
+   },*/
 
   getGeoData: function (queryDescriptor) {
     var adapters = {
@@ -159,10 +148,10 @@ Vizabi.Reader.extend('ddfcsv', {
     };
 
     var expectedGeoData = null;
-    for (var k in FILE_CACHED) {
-      if (FILE_CACHED.hasOwnProperty(k) &&
+    for (var k in ddfUtils.CACHE.FILE_CACHED) {
+      if (ddfUtils.CACHE.FILE_CACHED.hasOwnProperty(k) &&
         k.indexOf('ddf--list--geo--' + queryDescriptor.category) >= 0) {
-        expectedGeoData = FILE_CACHED[k];
+        expectedGeoData = ddfUtils.CACHE.FILE_CACHED[k];
         break;
       }
     }
@@ -174,7 +163,7 @@ Vizabi.Reader.extend('ddfcsv', {
       });
     }
 
-    DATA_CACHED['geo-' + queryDescriptor.category] = result;
+    ddfUtils.CACHE.DATA_CACHED['geo-' + queryDescriptor.category] = result;
     return result;
   },
 
@@ -193,104 +182,106 @@ Vizabi.Reader.extend('ddfcsv', {
     var _this = this;
     var expected = [];
 
-    FILE_CACHED[_this.indexPath].forEach(function (indexRecord) {
+    ddfUtils.CACHE.FILE_CACHED[_this.indexPath].forEach(function (indexRecord) {
       // todo: fix condition -> geo
       if (query.select.indexOf(indexRecord.value_concept) >= 0 &&
         (!query.where['geo.cat'] || query.where['geo.cat'].indexOf(indexRecord.geo) >= 0)) {
         var path = _this._ddfPath + '/' + indexRecord.file;
         // todo: swap...
-        CACHE.measureFileToName[indexRecord.value_concept] = path;
-        CACHE.measureNameToFile[path] = indexRecord.value_concept;
-        expected.push(_this.load(path));
+        ddfUtils.CACHE.measureFileToName[indexRecord.value_concept] = path;
+        ddfUtils.CACHE.measureNameToFile[path] = indexRecord.value_concept;
+        expected.push(ddfUtils.load(path));
       }
     });
 
     return expected;
-  },
-
-  getDimensionsDetails: function () {
-    var _this = this;
-    var expected = [];
-
-    FILE_CACHED[_this.dimensionPath].forEach(function (dimensionRecord) {
-      if (dimensionRecord.concept !== 'geo' && dimensionRecord.concept !== 'un_state') {
-        expected.push(_this.load(_this._ddfPath + '/ddf--list--geo--' + dimensionRecord.concept + '.csv'));
-      }
-    });
-
-    return expected;
-  },
-
-  _measureHashTransformer: function (measure, data) {
-    if (!measure) {
-      return data;
-    }
-
-    var hash = {};
-    data.forEach(function (d) {
-      if (!hash[d.geo]) {
-        hash[d.geo] = {};
-      }
-
-      if (!hash[d.geo][d.year]) {
-        hash[d.geo][d.year] = {};
-      }
-
-      hash[d.geo][d.year][measure] = d[measure];
-    });
-
-    return hash;
-  },
-
-  load: function (path) {
-    var _this = this;
-    if (!FILE_CACHED.hasOwnProperty(path) && !FILE_REQUESTED.hasOwnProperty(path)) {
-      fileReader(path, function (error, res) {
-        if (!res) {
-          console.log('No permissions or empty file: ' + path, error);
-        }
-
-        if (error) {
-          console.log('Error Happened While Loading CSV File: ' + path, error);
-        }
-
-        FILE_CACHED[path] = _this._measureHashTransformer(CACHE.measureNameToFile[path], res);
-        FILE_REQUESTED[path].resolve();
-      });
-    }
-
-    FILE_REQUESTED[path] = new Promise();
-
-    return FILE_REQUESTED[path];
   }
+
+  /*getDimensionsDetails: function () {
+   var _this = this;
+   var expected = [];
+
+   FILE_CACHED[_this.dimensionPath].forEach(function (dimensionRecord) {
+   if (dimensionRecord.concept !== 'geo' && dimensionRecord.concept !== 'un_state') {
+   expected.push(_this.load(_this._ddfPath + '/ddf--list--geo--' + dimensionRecord.concept + '.csv'));
+   }
+   });
+
+   return expected;
+   },
+
+   _measureHashTransformer: function (measure, data) {
+   if (!measure) {
+   return data;
+   }
+
+   var hash = {};
+   data.forEach(function (d) {
+   if (!hash[d.geo]) {
+   hash[d.geo] = {};
+   }
+
+   if (!hash[d.geo][d.year]) {
+   hash[d.geo][d.year] = {};
+   }
+
+   hash[d.geo][d.year][measure] = d[measure];
+   });
+
+   return hash;
+   },
+
+   load: function (path) {
+   var _this = this;
+   if (!ddfUtils.CACHE.FILE_CACHED.hasOwnProperty(path) && !ddfUtils.CACHE.FILE_REQUESTED.hasOwnProperty(path)) {
+   fileReader(path, function (error, res) {
+   if (!res) {
+   console.log('No permissions or empty file: ' + path, error);
+   }
+
+   if (error) {
+   console.log('Error Happened While Loading CSV File: ' + path, error);
+   }
+
+   ddfUtils.CACHE.FILE_CACHED[path] =
+   _this._measureHashTransformer(ddfUtils.CACHE.measureNameToFile[path], res);
+   ddfUtils.CACHE.FILE_REQUESTED[path].resolve();
+   });
+   }
+
+   ddfUtils.CACHE.FILE_REQUESTED[path] = new Promise();
+
+   return ddfUtils.CACHE.FILE_REQUESTED[path];
+   }*/
 });
 
 require('vizabi/build/dist/vizabi.css');
 
-function QueryDescriptor(query) {
-  var _this = this;
-  _this.query = query;
-  _this.geoCat = query.where['geo.cat'];
-  var result;
+/*function QueryDescriptor(query) {
+ var _this = this;
+ _this.query = query;
+ _this.geoCat = query.where['geo.cat'];
+ var result;
 
-  if (query.select.indexOf('geo.name') >= 0 || query.select.indexOf('geo.region') >= 0) {
-    _this.type = GEO;
-    _this.category = _this.geoCat[0];
-  }
+ if (query.select.indexOf('geo.name') >= 0 || query.select.indexOf('geo.region') >= 0) {
+ _this.type = GEO;
+ _this.category = _this.geoCat[0];
+ }
 
-  if (!result && query.where && query.where.time) {
-    if (query.where.time.length > 0 && query.where.time[0].length === 1) {
-      _this.type = MEASURES_TIME_FIXED;
-      _this.category = _this.geoCat[0];
-      _this.timeFrom = Number(query.where.time[0][0]);
-      _this.timeTo = Number(query.where.time[0][0]);
-    }
+ if (!result && query.where && query.where.time) {
+ if (query.where.time.length > 0 && query.where.time[0].length === 1) {
+ _this.type = MEASURES_TIME_FIXED;
+ _this.category = _this.geoCat[0];
+ _this.timeFrom = Number(query.where.time[0][0]);
+ _this.timeTo = Number(query.where.time[0][0]);
+ }
 
-    if (query.where.time.length > 0 && query.where.time[0].length === 2) {
-      _this.type = MEASURES_TIME_PERIOD;
-      _this.category = _this.geoCat[0];
-      _this.timeFrom = Number(query.where.time[0][0]);
-      _this.timeTo = Number(query.where.time[0][1]);
-    }
-  }
-}
+ if (query.where.time.length > 0 && query.where.time[0].length === 2) {
+ _this.type = MEASURES_TIME_PERIOD;
+ _this.category = _this.geoCat[0];
+ _this.timeFrom = Number(query.where.time[0][0]);
+ _this.timeTo = Number(query.where.time[0][1]);
+ }
+ }
+ }
+ */
---
+++ client/src/js/vizabi-ddf-utils.js
@@ -0,0 +1,124 @@
+var Promise = (require('./vizabi-extract-promise')).Promise;
+var fileReaders = require('./vizabi-file-readers');
+var fileReader = fileReaders.genericReader;
+
+var GEO = 1;
+var MEASURES_TIME_PERIOD = 2;
+var MEASURES_TIME_FIXED = 3;
+
+var CACHE = {
+  measureFileToName: {},
+  measureNameToFile: {},
+  FILE_CACHED: {},
+  FILE_REQUESTED: {},
+  DATA_CACHED: {}
+};
+
+exports.GEO = GEO;
+exports.MEASURES_TIME_PERIOD = MEASURES_TIME_PERIOD;
+exports.MEASURES_TIME_FIXED = MEASURES_TIME_FIXED;
+exports.CACHE = CACHE;
+
+function QueryDescriptor(query) {
+  this.query = query;
+  this.geoCat = query.where['geo.cat'];
+  var result;
+
+  if (query.select.indexOf('geo.name') >= 0 || query.select.indexOf('geo.region') >= 0) {
+    this.type = GEO;
+    this.category = this.geoCat[0];
+  }
+
+  if (!result && query.where && query.where.time) {
+    if (query.where.time.length > 0 && query.where.time[0].length === 1) {
+      this.type = MEASURES_TIME_FIXED;
+      this.category = this.geoCat[0];
+      this.timeFrom = Number(query.where.time[0][0]);
+      this.timeTo = Number(query.where.time[0][0]);
+    }
+
+    if (query.where.time.length > 0 && query.where.time[0].length === 2) {
+      this.type = MEASURES_TIME_PERIOD;
+      this.category = this.geoCat[0];
+      this.timeFrom = Number(query.where.time[0][0]);
+      this.timeTo = Number(query.where.time[0][1]);
+    }
+  }
+}
+
+function geoProcessing(ddfPath, dimensionPath, cb) {
+  getDimensions(dimensionPath).then(function () {
+    Promise
+      .all(getDimensionsDetails(ddfPath, dimensionPath))
+      .then(function () {
+        cb();
+      });
+  });
+}
+
+function getDimensions(dimensionPath) {
+  return load(dimensionPath);
+}
+
+function getDimensionsDetails(ddfPath, dimensionPath) {
+  var expected = [];
+
+s  CACHE.FILE_CACHED[dimensionPath].forEach(function (dimensionRecord) {
+    if (dimensionRecord.concept !== 'geo' && dimensionRecord.concept !== 'un_state') {
+      expected.push(load(ddfPath + '/ddf--list--geo--' + dimensionRecord.concept + '.csv'));
+    }
+  });
+
+  return expected;
+}
+
+function load(path) {
+  if (!CACHE.FILE_CACHED.hasOwnProperty(path) && !CACHE.FILE_REQUESTED.hasOwnProperty(path)) {
+    fileReader(path, function (error, res) {
+      if (!res) {
+        console.log('No permissions or empty file: ' + path, error);
+      }
+
+      if (error) {
+        console.log('Error Happened While Loading CSV File: ' + path, error);
+      }
+
+      CACHE.FILE_CACHED[path] = measureHashTransformer(CACHE.measureNameToFile[path], res);
+      CACHE.FILE_REQUESTED[path].resolve();
+    });
+  }
+
+  CACHE.FILE_REQUESTED[path] = new Promise();
+
+  return CACHE.FILE_REQUESTED[path];
+}
+
+function measureHashTransformer(measure, data) {
+  if (!measure) {
+    return data;
+  }
+
+  var hash = {};
+  data.forEach(function (d) {
+    if (!hash[d.geo]) {
+      hash[d.geo] = {};
+    }
+
+    if (!hash[d.geo][d.year]) {
+      hash[d.geo][d.year] = {};
+    }
+
+    hash[d.geo][d.year][measure] = d[measure];
+  });
+
+  return hash;
+}
+
+function getIndex(indexPath) {
+  return load(indexPath);
+}
+
+exports.QueryDescriptor = QueryDescriptor;
+exports.geoProcessing = geoProcessing;
+exports.getIndex = getIndex;
+exports.load = load;
